662c55942c162c1a3eaf8968bb9d212bcfc40bdc
Balakrishna Prodatoori <bala.csit@gmail.com>
2022-08-04 21:02:22 +0530

    Schema file is not required as the table is being created by hibernate process.

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-Create TABLE Todo
-(
-    id integer not null,
-    USER_NAME varchar(50) not null,
-    description varchar(255) not null,
-    date_of_completion DATE,
-    is_completed BOOLEAN,
-    primary key(id)
-);
