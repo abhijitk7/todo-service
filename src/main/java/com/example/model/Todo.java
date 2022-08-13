c3fd83766c6ea75a1722baf002a559d418b92aa4
Abhijit <abhikulkarni.1988@gmail.com>
2022-08-13 15:33:25 +0530

    Changes to date format and setting default value of isCompleted to false

--- src/main/java/com/example/model/Todo.java
+++ src/main/java/com/example/model/Todo.java
@@ -24,8 +24,8 @@ public class Todo implements Serializable {
 
     private String description;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date dateOfCompletion;
 
-    private Boolean isCompleted;
+    private Boolean isCompleted=false;
 }
