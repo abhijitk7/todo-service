1251c028525bd4cd22b2a9e7017fd16495fa6321
Abhijit <abhikulkarni.1988@gmail.com>
2022-08-13 20:33:07 +0530

    Todo list updated for project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,20 @@
-INSERT INTO Todo (id,description,user_name,date_of_completion,is_completed)
-VALUES (1001,'Learn Javascript','Abhijit',CURRENT_DATE(),false);
+INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
+VALUES (1001,'Abhijit','Implement OAuth2',CURRENT_DATE(),false);
 
 INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
-VALUES (1002,'Abhijit','Kafka',CURRENT_DATE(),false);
+VALUES (1002,'Abhijit','Implement JWT authentication',CURRENT_DATE(),false);
 
 INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
-VALUES (1003,'Abhijit','React',CURRENT_DATE(),false);
+VALUES (1003,'Abhijit','Implement API spring cloud API gateway',CURRENT_DATE(),false);
 
 INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
-VALUES (1004,'Abhijit','System design',CURRENT_DATE(),false);
+VALUES (1004,'Abhijit','Implement Eureka service discovery',CURRENT_DATE(),false);
+
+INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
+VALUES (1005,'Abhijit','Deploy UI application to GCP',CURRENT_DATE(),false);
+
+INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
+VALUES (1006,'Abhijit','Deploy service to GCP',CURRENT_DATE(),false);
+
+INSERT INTO Todo (id,user_name,description,date_of_completion,is_completed)
+VALUES (1007,'Abhijit','Need to implement Toasters for Success and Failure',CURRENT_DATE(),false);
